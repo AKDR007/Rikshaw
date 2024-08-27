d5629a428fd781b89c80ada2c9d7445934b8ccc8
AKDR007 <ajhaykumar007n@gmail.com>
2024-08-27 16:50:02 +0530

    Updated yml, Updated Network Image

--- lib/TypeFaceSystem.dart
+++ lib/TypeFaceSystem.dart
@@ -8,5 +8,5 @@ class TextStyles {
   // Google Font Style
 
   static TextStyle notoSansMono = GoogleFonts.notoSansMono(
-      fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black);
+      fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:rikshaw/Login.dart';
 import 'dart:async';
@@ -34,10 +35,13 @@ class SplashScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // Add your splash screen logo or image here
-            Image.asset(
-              'assets/Icons/App_Icon.png', // Replace with your logo
-              height: 120,
-            ),
+            // Image.asset(
+            //   'assets/Icons/App_Icon.png', // Replace with your logo
+            //   height: 120,
+            // ),
+            CachedNetworkImage(
+                imageUrl:
+                    "https://www.shutterstock.com/image-vector/vector-design-auto-rickshaw-driver-260nw-2316837373.jpg"),
             SizedBox(height: 20),
             // You can also add a loading indicator or text
             CircularProgressIndicator(
